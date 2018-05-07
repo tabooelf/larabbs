3be50e2b91cdc8dd127c32d232896abdb758675e
tabooelf <tabooelf@qq.com>
2018-05-07 09:59:34 +0000

    Topic detail page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -41,7 +41,7 @@ class TopicsController extends Controller
         $topic->save();
         // var_dump($topic);
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', '创建成功.');
 	}
 
 	public function edit(Topic $topic)
@@ -56,7 +56,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功.');
 	}
 
 	public function destroy(Topic $topic)
@@ -64,7 +64,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('success', '删除成功.');
 	}
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader){
